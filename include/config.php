186aa72ff6cb014b68154235a9fdf0a0db571a3a
protomods <Hello157@icloud.com>
2016-11-29 19:54:51 -0800

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,8 +1,8 @@
 <?php
 define("DB_PDODRIVER", "mysql");
-define("DB_HOST", "localhost");
-define("DB_DATABASE", "mydatabase");
-define("DB_USERNAME", "dbuser");
-define("DB_PASSWORD", "dbpassword");
+define("DB_HOST", "192.168.1.4");
+define("DB_DATABASE", "https://protomods.github.io/Jcode/");
+define("DB_USERNAME", "jcode");
+define("DB_PASSWORD", "goober123");
 
 define("SHORTURL_PREFIX", "http://localhost/r.php?c=");
